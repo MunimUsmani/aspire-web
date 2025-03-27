a3ca599bf1c774ace0819d7719fe6d75a0fec45c
MunimUsmani <abdulmunimusmani152@gmail.com>
2025-03-28 00:30:37 +0500

    contact route made

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/footer";
+import Navbar from "../components/navbar";
+
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Home;
