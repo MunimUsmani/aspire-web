433d9c7a7af8647da9216d8afd5a43af115a5b67
MunimUsmani <abdulmunimusmani152@gmail.com>
2025-04-07 22:06:34 +0500

    client-say component made

---
+++ src/app/components/client-say.tsx
@@ -0,0 +1,13 @@
+const Clients = () => {
+    return (
+      <section className="w-full py-16 md:py-24 lg:py-32">
+        <div className="container mx-auto px-4 md:px-6">
+          <h1 className="text-5xl md:text-6xl lg:text-6xl font-bold text-center text-[#c19a6b]">
+            What Our Clients Say
+          </h1>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Clients;
