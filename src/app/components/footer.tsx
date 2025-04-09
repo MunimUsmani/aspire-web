af66941acb4f3eabe907172c83a5ddd93ee315b5
MunimUsmani <abdulmunimusmani152@gmail.com>
2025-04-10 00:54:54 +0500

    footer done

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -80,7 +80,7 @@ const Footer = () => {
       {/* Logo */}
       <div className="flex justify-center mb-5">
         <Image 
-          src="/Logo.png" 
+          src="/logo/logo.png" 
           alt="Aspire HR Consultants" 
           width={140} 
           height={50}
